a6706481b3f58499e1e02769771963ed525ca3a7
mhuryanov <mhuryanov@gmail.com>
2022-10-03 22:17:25 +0300

    get theme list

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,54 @@
+function addThemeList(theme) {
+    $('#themeList').append(`
+        <li class="menu-item" title="${theme.title}">
+            <a href="#" onclick='moveToSnapshot(${theme.id})'>
+                <div class="word-name">${snapshot.name}</div>
+                &nbsp;
+            </a>
+            <div class="action">
+                <button class="btn button-icon button-icon-small icon-photo" title="Update with current view" onclick='updateSnapshot(${snapshot.id})'></button>
+                <button class="btn button-icon button-icon-small icon-trash" title="Remove" onclick='removeSnapshot(${snapshot.id})'></button>
+            </div>
+        </li>
+    `);
+}
+
+async function genList(themes) {
+    var wordCount
+    for(i=0; i<themes.length; i++) {
+        const children = await themes[i].getChildren()
+        for(j=0; j<children.length; j++) {
+            var text = stripHtml(children[j].content)
+            .replace(/[^A-Za-z0-9]/g, ' ')
+            .toLowerCase()
+            .replace(/\s\s+/g, ' ')
+            var words = text.split(' ');
+            for (word of words) {
+                // Get word count in this widget
+                if (stopList.indexOf(word) == -1) {
+                    if(wordCount[word]) wordCount[word] ++
+                    else wordCount[word] = 0
+                }
+            }
+        }
+    }
+}
+
 function loadTabTheme() {
-    
+    toggleLoading(true);
+    getThemes().then((themes) => {
+        $("#themeList").html('')
+        if(themes && themes.length) {
+            themes.forEach((theme) => {
+                addThemeList(theme)
+            })
+        }
+        toggleLoading(false)
+    })
 }
 
 async function getThemes() {
-
+    return await miro.board.get({type:['frame']})
 }
 
 $('#addTheme').on('click', async () => {
