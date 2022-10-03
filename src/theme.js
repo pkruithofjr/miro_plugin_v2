945ea37d25b650dac100351fddab43a6f1f19e7a
mhuryanov <mhuryanov@gmail.com>
2022-10-03 16:20:16 +0300

    update theme tab

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+function loadTabTheme() {
+    
+}
+
+$('#addTheme').on('click', async () => {
+    toggleLoading(true);
+
+    await miro.board.setAppData('focusedThemeName','Theme')
+
+    var stickies = await getStickies();
+    var tags = await getTags();
+    var themes = await getThemes();
+
+    miro.board.ui.openModal({
+        url: 'setThemeNameModal.html',
+        width: 400,
+        height: 250,
+        fullscreen: false,
+    }).then(() => {
+
+    })
+
+    toggleLoading(false);
+})
