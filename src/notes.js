ee5740086c7f1aeca30d636abdee3c168dedcae8
mhuryanov <mhuryanov@gmail.com>
2022-10-06 18:52:20 +0300

    update add sticky tab

--- src/notes.js
+++ src/notes.js
@@ -3,7 +3,17 @@ function loadTabNotes() {
     $("#tagViewList").empty()
     getTags().then((tags) => {
         tags.forEach((tag) => {
-            $("#tagViewList").append(`<div class="cs1 ce12 placeholder">${tag}</div>`)
+            $("#tagViewList").append(
+                `<div class="cs1 ce12 placeholder">
+                    <button class="button button-secondary tag-button" style="background-color:${tag.color};">
+                        <span class="icon-tag"></span>
+                        ${tag.title}
+                    <button>
+                </div>`)
         })
     })
 }
+
+$(".tag-button").click(function() {
+    this.classList.add('selected')
+})
