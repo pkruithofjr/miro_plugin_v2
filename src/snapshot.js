21d14cecc34aec0385f03a47d92d4a4966aceb69
mhuryanov <mhuryanov@gmail.com>
2022-10-14 02:42:29 +0300

    update snapshot tab

--- src/snapshot.js
+++ src/snapshot.js
@@ -93,10 +93,6 @@ async function moveToSnapshot(snapshotId) {
             }
             newNote.tagIds = res
             await miro.board.createStickyNote(newNote)
-            if(parentId) {
-                const selected_theme = await miro.board.getById(newThemes[oldThemes.indexOf(parentId)])
-                await selected_theme.add(newNote)
-            }
         }
         toggleLoading(false);
     }
